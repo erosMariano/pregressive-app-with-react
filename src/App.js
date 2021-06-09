d4530409836d85a2a755f48971cace841fc7788a
erosMariano <erosmariano1@gmail.com>
2021-06-09 15:23:24 -0300

    started setup + components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StylesProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import GlobalStyle from "./commons/styles/global-style";
+import Main from "./containers/Main/Index";
+
+function App() {
+  return (
+    <div className="App">
+      <StylesProvider injectFirst>
+        <CssBaseline/>
+        <GlobalStyle/>
+        
+        <Main/>
+
+
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
